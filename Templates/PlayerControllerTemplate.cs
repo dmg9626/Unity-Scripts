26ccd4acf90b4aade5b9736d296b4de82a85faff
Drew Graham <dmg9626@gmail.com>
2017-03-21 20:46:18 -0400

    added Templates folder and PlayerControllerTemplate
    - created Templates folder
    - added PlayerControllerTemplate.cs, a script with basic 4-directional controls for a player in a 2D game

---
+++ Templates/PlayerControllerTemplate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private GameObject _player;
+    private float _moveSpeed;
+	// Use this for initialization
+	void Start ()
+    {
+        _player = gameObject;
+        _moveSpeed = 5  ;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _player.transform.position = new Vector2(_player.transform.position.x, _player.transform.position.y + (Time.deltaTime * _moveSpeed));
+        if (Input.GetKey(KeyCode.A))
+            _player.transform.position = new Vector2(_player.transform.position.x - (Time.deltaTime * _moveSpeed), _player.transform.position.y);
+        if (Input.GetKey(KeyCode.S))
+            _player.transform.position = new Vector2(_player.transform.position.x, _player.transform.position.y - (Time.deltaTime * _moveSpeed));
+        if (Input.GetKey(KeyCode.D))
+            _player.transform.position = new Vector2(_player.transform.position.x + (Time.deltaTime * _moveSpeed), _player.transform.position.y);
+    }
+}
